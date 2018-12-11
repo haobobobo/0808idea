07e9b22caa95594d7549c64e7f31d5655d78d4eb
haobobobo666 <haobobobo666666@163.com>
2018-12-11 21:02:34 +0800

    update main methd

--- test/src/main/java/TestGit.java
+++ test/src/main/java/TestGit.java
@@ -2,7 +2,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("111111111");
         System.out.println("fgkbjkfdgbhb fgbkfbgkhfdvg" +
-                "jyefbgidfbgkd");
+                "jyefb");
 
     }
 }
